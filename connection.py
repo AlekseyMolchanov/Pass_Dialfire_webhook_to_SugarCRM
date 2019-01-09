55c52d39aac4a919c694fa872a50d93e7c9b2ae0
Aleksey Molchanov <molchanovav@gmail.com>
2019-01-10 02:16:51 +0300

    pep8

--- connection.py
+++ connection.py
@@ -8,10 +8,10 @@ from sugarcrm import Session
 def sugar_crm_connect():
     session = None
     try:
-        auth=Session.local_auth
+        auth = Session.local_auth
 
         if os.environ.get('CIRCLECI') or os.environ.get('REMOTE'):
-            auth=Session.remote_auth
+            auth = Session.remote_auth
 
         url = os.environ['SUGAR_CRM_URL']
         username = os.environ['SUGAR_CRM_USERNAME']
@@ -23,17 +23,8 @@ def sugar_crm_connect():
 
 
 def server_settings():
-    _connect = dict()
-    try:
-        host = os.environ.get('SERVER_HOST') or '0.0.0.0'
-        port = os.environ.get('SERVER_PORT') or '8080'
-        debug = bool(int(os.environ.get('SERVER_DEBUG') or 0))
-        _connect = dict(
-            debug=debug, 
-            host=host, 
-            port=port
-        )
-    except KeyError as exception:
-        pass
-    return _connect    
-   
+    host = os.environ.get('SERVER_HOST') or '0.0.0.0'
+    port = os.environ.get('SERVER_PORT') or '8080'
+    debug = bool(int(os.environ.get('SERVER_DEBUG') or 0))
+    _connect = dict(debug=debug, host=host, port=port)
+    return _connect
