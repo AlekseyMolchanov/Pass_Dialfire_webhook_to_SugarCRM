62297748bf58df75f8ac20cf97067dd4c602c1d2
Aleksey Molchanov <molchanovav@gmail.com>
2019-03-05 23:29:19 +0300

    fixed mistake in sh script webhook_start

--- webhook_start.sh
+++ webhook_start.sh
@@ -10,4 +10,4 @@
 # export SUGAR_CRM_HOST='crm.intra:192.168.0.200'
 
 docker build --rm -f "Dockerfile" -t dialfire_webhook:latest .
-docker run -it -d --rm -p 8080:8080 -e SUGAR_CRM_URL -e SUGAR_CRM_USERNAME -e SUGAR_CRM_PASSWORD -e SUGAR_CRM_ASSIGNED_USER_ID -e no_proxy --name dialfire_webhook --add-host=$SUGAR_CRM_HOST dialfire_webhook
+docker run -it -d --rm -p 8080:8080 -e SUGAR_CRM_URL -e SUGAR_CRM_USERNAME -e SUGAR_CRM_PASSWORD -e SUGAR_CRM_ASSIGNED_USER_ID -e no_proxy --name dialfire_webhook --add-host=${SUGAR_CRM_HOST} dialfire_webhook
